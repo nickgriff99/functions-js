9757aebf0f8aeac013710adde72db9047c8c82db
Nicholas Griffiths <nichg999@gmail.com>
2025-01-25 14:57:12 -0700

    finished exercise 3

--- exercises/e3.js
+++ exercises/e3.js
@@ -16,7 +16,14 @@
 
 // Your code goes here...
 
-
+function doesArrayIncludeItemsBetweenVals(arr, val1, val2) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > val1 && arr[i] < val2) {
+      return true;
+    }
+  }
+  return false;
+}
 
 
 /**
@@ -36,7 +43,26 @@
 
 // Your code goes here...
 
+function getValueWithConditionOne(num1, num2) {
+  if (num1 === 40 && num2 === 40) {
+    return num1 + num2;
+  }
+  return (num1 + num2) * 2;
+}
 
+const getValueWithConditionTwo = (num1, num2) => {
+  if (num1 === 40 && num2 === 40) {
+    return num1 + num2;
+  }
+  return (num1 + num2) * 2;
+}
+
+const getValueWithConditionThree = function(num1, num2) {
+  if (num1 === 40 && num2 === 40) {
+    return num1 + num2;
+  }
+  return (num1 + num2) * 2;
+}
 
 
 // === TEST YOURSELF ===
