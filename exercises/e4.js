9c44a0ee8bbd2bcfc1deb46a262b861e18dd9c7c
Nicholas Griffiths <nichg999@gmail.com>
2025-01-25 15:06:43 -0700

    finished exercise 4

--- exercises/e4.js
+++ exercises/e4.js
@@ -9,9 +9,11 @@
  * splitFirstAndLastNames('John Smith') => ['John', 'Smith']
  */
 
-function splitFirstAndLastNames(str) {
+const splitFirstAndLastNames = (str) =>{
   return str.split(' ');
 }
+
+splitFirstAndLastNames('John Smith');
 
 
 /** =========================
@@ -20,9 +22,11 @@ function splitFirstAndLastNames(str) {
  * personAge({ id: 1, name: 'Someone', age: 32}) => 32
  */
 
-const personAge = function(personObject) {
+const personAge = (personObject) => {
   return personObject.age;
 }
+
+personAge({ id: 1, name: 'Someone', age: 32});
 
 
 /** =========================
@@ -32,9 +36,11 @@ const personAge = function(personObject) {
  * isNameInArray(['Jon', 'Michael', 'Andrey'], 'James') => false;
  */
 
-const isNameInArray = function(arr, name) {
+const isNameInArray = (arr, name) => {
   return arr.includes(name);
 }
+
+isNameInArray(['Jon', 'Michael', 'Andrey'], 'Michael');
 
 
 /** =========================
@@ -44,7 +50,7 @@ const isNameInArray = function(arr, name) {
  * !!! Hint: There are two functions to refactor !!!
  */
 
-const logSecondsUpToMax = function(max) {
+const logSecondsUpToMax = (max) => {
   let i = 0;
   const timer = setInterval(function() {
     if (i < max) {
@@ -54,6 +60,8 @@ const logSecondsUpToMax = function(max) {
     }
   }, 1000);
 }
+
+logSecondsUpToMax(5);
 
 
 
